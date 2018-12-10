3d2cbbecd9330fbc677ca90fdfcff9df401bebce
David Raymond <pieceofchum@yahoo.com>
2018-12-09 20:01:06 -0500

    Minor change to point to a different contract

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CardSeriesFactory from './build/CardSeriesFactory';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CardSeriesFactory.interface),
-  '0x599b011b96F2B5E73F24B01F8662e0A958a16b2f'
+  '0x9CF0dCf4397F3B16938ACEE141c484001b7e937b'
 );
 
 export default instance;
